c287b2cdc018f6b4fb84fecf939e4d26cdf352fa
Sean Altemose <sean@seanaltemose.com>
2019-01-07 16:26:31 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,5 @@
 class Post < ActiveRecord::based
   validates :title, presence: true
   validates :content, length: { minimum: 100 }
+  validates :category, inclusion: { in: %w{Fiction Non-Fiction}}
+end 
